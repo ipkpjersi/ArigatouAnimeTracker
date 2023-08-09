017b0071d2030a31c693f4543501bca778bd626d
ipkpjersi <ipkpjersi@gmail.com>
2023-08-08 22:15:05 -0400

    Added DataTables with example

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,3 +30,11 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+
+import 'datatables.net';
+import 'datatables.net-responsive';
+import 'datatables.net-dt/css/jquery.dataTables.css';
+import 'datatables.net-responsive-dt/css/responsive.dataTables.min.css';
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,51 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
+                <div class="p-4">
+                    <p>Datatables example: </p>
+                    <table style="width:100%" id="myTable" class="display text-gray-900 dark:text-gray-100">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Age</th>
+                                <th>Position</th>
+                                <th>Salary</th>
+                                <th>Tenure</th>
+                                <th>Stock Options</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Tiger Nixon</td>
+                                <td>61</td>
+                                <td>System Architect</td>
+                                <td>80,000</td>
+                                <td>12 years</td>
+                                <td>Yes</td>
+                            </tr>
+                            <tr>
+                                <td>Garrett Winters</td>
+                                <td>63</td>
+                                <td>Accountant</td>
+                                <td>80,000</td>
+                                <td>15 years</td>
+                                <td>Yes</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
+    <script type="module">
+        $(document).ready(function () {
+        $('#myTable').DataTable({
+            responsive: true,
+            columnDefs: [
+                { responsivePriority: 1, targets: 0 }, //Highest priority to first column
+                { responsivePriority: 2, targets: 1 }, //Next priority to second column
+            ]
+        });
+    });
+    </script>
 </x-app-layout>
