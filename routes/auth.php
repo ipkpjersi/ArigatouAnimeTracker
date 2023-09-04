fcd982399d332ec8e32c524e35f9cb67707a693f
ipkpjersi <ipkpjersi@gmail.com>
2023-09-04 14:51:01 -0400

    Reduced register throttle limit

--- routes/auth.php
+++ routes/auth.php
@@ -15,7 +15,7 @@ Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store'])->middleware('throttle:50,10');
+    Route::post('register', [RegisteredUserController::class, 'store'])->middleware('throttle:5,10');
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
