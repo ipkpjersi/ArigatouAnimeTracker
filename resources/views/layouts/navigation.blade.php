f21b7d57b2dc935cb0608a3caeec78ad1e1c3bc3
ipkpjersi <ipkpjersi@gmail.com>
2023-09-04 19:31:09 -0400

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,7 +44,7 @@
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                               <div class="flex items-center">
+                                <div class="flex items-center">
                                     <img onerror="this.onerror=null; this.src='/img/notfound.gif';" class="rounded-lg shadow-md h-8 w-8 mr-2" src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->username ?? "" }}" />
                                     <div>{{ Auth::user()->username ?? "" }}</div>
                                 </div>
