122b6b62bd8c71845d15628c4b768c818dd43902
ipkpjersi <ipkpjersi@gmail.com>
2023-09-04 14:52:58 -0400

    Added forgot password reset password throttling

--- routes/auth.php
+++ routes/auth.php
@@ -25,13 +25,13 @@ Route::middleware('guest')->group(function () {
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->middleware('throttle:5,10')
                 ->name('password.email');
 
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
                 ->name('password.reset');
 
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
+    Route::post('reset-password', [NewPasswordController::class, 'store'])->middleware('throttle:5,10')
                 ->name('password.store');
 });
 
