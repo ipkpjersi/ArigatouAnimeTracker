56923f194320401763e7c1a2616a509266befd5a
ipkpjersi <ipkpjersi@gmail.com>
2023-09-10 01:34:19 -0400

    Improved categories list styling

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -5,17 +5,19 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <ul>
+                    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
                         @foreach($categories as $category)
-                            <li>
-                                <a href="{{ route('anime.category', ['category' => $category]) }}">{{ $category }}</a>
-                            </li>
+                            <div class="text-center p-2 rounded-lg">
+                                <a href="{{ route('anime.category', ['category' => $category]) }}" class="block p-2 rounded bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600">
+                                    {{ $category }}
+                                </a>
+                            </div>
                         @endforeach
-                    </ul>
+                    </div>
                 </div>
             </div>
         </div>
