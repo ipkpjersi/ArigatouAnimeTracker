4fcec52304eced8fffe2a1b95a434e3b5a204971
ipkpjersi <ipkpjersi@gmail.com>
2023-10-07 22:03:14 -0400

    Added clear anime image zip files command

---
+++ app/Console/Commands/ClearAnimeImageZipFiles.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+class ClearAnimeImageZipFiles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:clear-anime-image-zip-files';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deletes the zip files inside picture and thumbnail folders while retaining the folder structure.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->info("Starting to clear image zip files...");
+
+        $folders = [
+            public_path('picture'),
+            public_path('thumbnail')
+        ];
+
+        foreach ($folders as $folder) {
+            $this->deleteZipFiles($folder);
+        }
+
+        $this->info("Image zip files cleared.");
+    }
+
+    /**
+     * Deletes zip files within the specified folder while retaining the folder structure.
+     *
+     * @param string $folder
+     * @return void
+     */
+    private function deleteZipFiles($folder)
+    {
+        if (is_dir($folder)) {
+            $it = new RecursiveDirectoryIterator($folder, RecursiveDirectoryIterator::SKIP_DOTS);
+            $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
+            foreach ($files as $file) {
+                if (!$file->isDir() && $this->isZipFile($file)) {
+                    unlink($file->getRealPath());
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks if a file is a zip file based on its extension.
+     *
+     * @param \SplFileInfo $file
+     * @return bool
+     */
+    private function isZipFile($file)
+    {
+        return strtolower($file->getExtension()) === 'zip';
+    }
+}
