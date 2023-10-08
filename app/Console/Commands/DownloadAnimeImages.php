8bf29f9c8007a77cacb2bb94a477e69d15e20a4e
ipkpjersi <ipkpjersi@gmail.com>
2023-10-07 23:44:11 -0400

    Re-use anime image zip command

--- app/Console/Commands/DownloadAnimeImages.php
+++ app/Console/Commands/DownloadAnimeImages.php
@@ -40,9 +40,7 @@ class DownloadAnimeImages extends Command
             $result = $animeImageDownloadService->downloadImages($logger);
             $duration = round($result['duration'], 2);
             $this->info("Downloaded {$result['count']} out of {$result['totalImages']} images for {$result['total']} anime records successfully in {$duration} seconds.");
-            $this->info("Starting to zip anime images...");
-            $animeImageDownloadService->zipImages();
-            $this->info("Zipping of anime images completed successfully.");
+            $this->call('artisan:zip-anime-images');
         } catch (\Exception $e) {
             $this->error('An error occurred during the anime image downloads fetch: ' . $e->getMessage());
         }
