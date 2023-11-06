bf8fa6160292eaf20dfee69e35570c6061a66730
ipkpjersi <ipkpjersi@gmail.com>
2023-11-05 23:00:32 -0500

    Added user_friends junction table and relationship

---
+++ database/migrations/2023_11_05_225247_create_user_friends_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_friends', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('friend_user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+
+            // Unique constraint to ensure a user cannot add the same friend multiple times
+            $table->unique(['user_id', 'friend_user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_friends');
+    }
+};
