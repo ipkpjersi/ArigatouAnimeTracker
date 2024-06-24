107a67d29cabd559c915e6b63f9b32ce5ac9d855
ipkpjersi <ipkpjersi@gmail.com>
2024-06-24 16:50:22 -0400

    Added extra MAL fields

---
+++ database/migrations/2024_06_24_204440_add_source_background_recommendations_studios_broadcast_and_related_anime_to_anime_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('anime', function (Blueprint $table) {
+            $table->string('source')->nullable();
+            $table->text('background')->nullable();
+            $table->text('recommendations')->nullable();
+            $table->string('studios')->nullable();
+            $table->string('broadcast')->nullable();
+            $table->text('related_anime')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('anime', function (Blueprint $table) {
+            $table->dropColumn('source');
+            $table->dropColumn('background');
+            $table->dropColumn('recommendations');
+            $table->dropColumn('studios');
+            $table->dropColumn('broadcast');
+            $table->dropColumn('related_anime');
+        });
+    }
+};
