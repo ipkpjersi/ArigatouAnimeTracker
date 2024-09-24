edaf5b3ed8a83bef68b62355a88e8e3fa8bd2bc0
ipkpjersi <ipkpjersi@gmail.com>
2024-09-23 21:16:59 -0400

    Fixed rating and duration not nullable

---
+++ database/migrations/2024_09_24_011431_make_rating_and_duration_nullable.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::table('anime', function (Blueprint $table) {
+            $table->string('rating')->nullable()->change();
+            $table->integer('duration')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $table->string('rating')->nullable(false)->change();
+        $table->integer('duration')->nullable(false)->change();
+    }
+};
