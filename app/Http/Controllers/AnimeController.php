b8c64c18811629214ca582214fed1b4131165277
ipkpjersi <ipkpjersi@gmail.com>
2024-12-23 17:31:31 -0500

    Fixed anime list pagination size

--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -449,7 +449,7 @@ class AnimeController extends Controller
         if ($request->has('showallanime') && $request->input('showallanime') === '1' && Auth::user() !== null && strtolower(Auth::user()->username) === strtolower($user->username)) {
             $showAllAnime = true;
         }
-        $pagination = $user->anime_list_pagination_size ?? 15;
+        $pagination = Auth::user()?->anime_list_pagination_size ?? $user->anime_list_pagination_size ?? 15;
         if ($request->has('size') || $request->has('pageSize') || $request->has('pagesize')) {
             $pagination = $request->get('size') ?? $request->get('pageSize') ?? $request->get('pagesize');
             $pagination = max(1, min((int) $pagination, 1000)); //Clamp between 1 and 1000
